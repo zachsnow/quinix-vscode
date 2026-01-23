ae37057c2a56f0dd6dbf764cb840a5daa20d9613
Zach Snow <z@zachsnow.com>
2026-01-23 00:13:30 -0700

    Make install.sh more robust.

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+set -e
+
+bun install
+bun run validate
+
 rm -rf ~/.vscode/extensions/quinix-vscode
 mkdir -p ~/.vscode/extensions/quinix-vscode
 git archive HEAD | tar -x -C ~/.vscode/extensions/quinix-vscode
+
+echo "Installed to ~/.vscode/extensions/quinix-vscode"
